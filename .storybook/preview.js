91ddce4835767f2048900075fdcf3f1fb677b07b
fgbyte <fer.garciabayon@gmail.com>
2023-11-14 18:11:49 -0500

    add storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,18 @@
+import '/dist/css/main.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
+
+
+/** @type { import('@storybook/html').Preview } */
+const preview = {
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
