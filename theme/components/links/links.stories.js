10cc01373aceaf05e971ec93f24bd62a132ab66d
fgbyte <fer.garciabayon@gmail.com>
2023-11-15 13:26:27 -0500

    workflow

---
+++ theme/components/links/links.stories.js
@@ -0,0 +1,5 @@
+export const link1 = `<a class="link" href="#">link</a>`
+
+export const link2 = `<a class="link--white" href="#">link white</a>`
+
+export const link3 = `<a class="link-arrow" href="#">link arrow <span></span></a>`
