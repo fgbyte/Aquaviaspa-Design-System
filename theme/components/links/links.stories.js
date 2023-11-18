1d977431ddcb53fb2a1dbfa6d076f890b9c02ea3
fgbyte <fer.garciabayon@gmail.com>
2023-11-18 15:37:50 -0500

    working on links

--- theme/components/links/links.stories.js
+++ theme/components/links/links.stories.js
@@ -1,5 +1,28 @@
-export const link1 = `<a class="link" href="#">link</a>`
+export const link1 = `<a class="link" href="#">Link</a>`
 
-export const link2 = `<a class="link--white" href="#">link white</a>`
+export const link2 = `<a class="link link--white" href="#">Link white</a>`
 
-export const link3 = `<a class="link-arrow" href="#">link arrow <span></span></a>`
+export const link3 = `<a class="link link-menu" href="#">Link menu</a>`
+
+export const link4 = `<a class="link-menu link-menu--upper" href="#">Link menu uppercase</a>`
+
+export const link5 =
+    `
+<a class="link-menu-arrow" href="#">
+        Link menu arrow
+        <span><img src="/dist/assets/icons/link-menu-arrow.svg" alt=""></span>
+    </a>
+    `
+
+export const link6 = `
+<a class="link-menu-arrow link-menu-arrow--upper" href="#">
+Link menu arrow upper
+<span><img src="/dist/assets/icons/link-menu-arrow.svg" alt=""></span>
+</a>`
+
+export const link7 = `
+<a class="link-menu-arrow  link-menu-arrow--lang" href="#">
+        <img src="/dist/assets/icons/link-menu-arrow--lang.svg"></img>España
+        <span><img src="/dist/assets/icons/link-menu-arrow--lang-arrow.svg" alt=""></span>
+    </a>
+`
