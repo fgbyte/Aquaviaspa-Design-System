ef0913eadefe0c3300847e846ec2a1aa9b0f5ac3
fgbyte <fer.garciabayon@gmail.com>
2023-11-23 11:03:45 -0500

    button stories

--- theme/components/buttons/buttons.stories.js
+++ theme/components/buttons/buttons.stories.js
@@ -1,2 +1,28 @@
-export const btn1 = `    <button type="button" class="btn-cta">test</button>
+export const btn1 = `
+<button type="button" class="btn-ficha">Descargue el catalogo</button>
+`
+export const btn2 = `
+<button type="button" class="btn-gray">SOLICITE UN PRESUPUESTO</button>
+`
+
+export const btn3 = `<button type="button" class="btn-cta"><span>Descubre nuestros spas</span>
+<div class="d-flex align-items-center">
+<div class="btn-cta__arrow"></div>
+<svg xmlns="http://www.w3.org/2000/svg" width="7.026" height="20.162" viewBox="0 0 9.026 20.162">
+<g id="Grupo_12494" class="btn-cta__arrow-svg" data-name="Grupo 12494" transform="translate(-326.008 -160.675)">
+      <line id="Línea_18" data-name="Línea 18" x1="8.24" y1="10.486" transform="translate(326.401 160.984)"
+        stroke-width="1" />
+        <line id="Línea_19" data-name="Línea 19" y1="9.029" x2="8.24" transform="translate(326.401 171.471)"
+        stroke-width="1" />
+        </g>
+  </svg>
+</div>
+</button>
+`
+export const btn4 = `
+<button type="button" class="btn-ficha-home px-5">Descargar</button>
+`
+
+export const btn5 = `
+<button type="button" class="btn-blue">PEDIR PRESUPUESTO</button>
 `
