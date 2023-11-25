fd3d3b031af22d1aafd598e9b3be4067700dd93f
fgbyte <fer.garciabayon@gmail.com>
2023-11-25 14:48:15 -0500

    dropdowns done

---
+++ theme/components/dropDowns/dropDowns.stories.js
@@ -0,0 +1,105 @@
+export const drop1 = `
+<div class="dropdown">
+<a class="link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown"
+  aria-expanded="false">
+  Capacidad
+  <span><img src="/dist/assets/icons/link-menu-arrow--lang-arrow.svg" alt=""></span>
+</a>
+<ul class="dropdown-menu bg-secondary" aria-labelledby="navbarDropdown">
+  <li><a class="dropdown-item" href="#">2-3</a></li>
+  <li><a class="dropdown-item" href="#">4-5</a></li>
+  <!-- <li>
+        <hr class="dropdown-divider">
+      </li> -->
+  <li><a class="dropdown-item" href="#">6-7*</a></li>
+</ul>
+</div>
+`
+
+export const drop2 = `
+<select class="dropdown-maps" name="select">
+<option value="value1" selected>Distancia</option>
+<option value="value2">Value 2</option>
+<option value="value3">Value 3</option>
+</select>
+`
+
+export const drop3 = `
+<details class="dropdown-card">
+<summary>Especificaciones Generales</summary>
+<div class="details-inner">
+  <ul>
+    <li>
+      <div>
+        <p>Dimensiones del Spa (cm):</p>
+        <span>198x198x76</span>
+      </div>
+    </li>
+    <li class="bg-danger">
+      <div>
+        <p> Número de plazas del Spa (sentados / tumbados):</p>
+        <span>4 (4/-)</span>
+      </div>
+    </li>
+    <li>
+      <div>
+        <p>Volumen de agua de los Spas (litros):</p>
+        <span>975</span>
+      </div>
+    </li>
+    <li class="bg-danger">
+      <div>
+        <p>Peso del Spa: (vacío - lleno):</p>
+        <span>220 Kg / 1.195 Kg</span>
+      </div>
+    </li>
+    <li>
+      <div>
+        <p>Bomba de masaje del Spa:</p>
+        <span>1.800 W / 2,5 CV</span>
+      </div>
+    </li>
+    <li class="bg-danger">
+      <div>
+        <p>Bomba soplante del Spa:</p>
+        <span>1.150 W</span>
+      </div>
+    </li>
+    <li>
+      <div>
+        <p> Bomba de filtración del Spa:</p>
+        <span>200 W</span>
+      </div>
+    </li>
+    <li class="bg-danger">
+      <div>
+        <p>Calefacción eléctrica del Spa:</p>
+        <span>3.000 W</span>
+      </div>
+    </li>
+    <li>
+      <div>
+        <p>Potencia requerida:</p>
+        <span> Low Amp (W) / (A) a 230 V: 3.400 W / 14,6 A </span>
+      </div>
+    </li>
+    <li class="bg-danger">
+      <div>
+        <p> Voltaje de los Spas:</p>
+        <span> 230 V / 400 V III</span>
+      </div>
+    </li>
+  </ul>
+</div>
+</details>
+`
+
+
+export const drop4 = `
+<details class="dropdown-podemos">
+<summary>¿Podemos ayudarle?</summary>
+  <div class="details-inner">
+        <form action="">form__footer-podemos</form>
+  </div>
+</details>
+`
