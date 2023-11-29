cc84455fe9f79b84eb7016b4a2aca4f92a4050ff
fgbyte <fer.garciabayon@gmail.com>
2023-11-29 12:44:33 -0500

    modify dropdown-card

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,1 +1,15 @@
 //el js de la web
+
+
+
+//dropdown-card lg disposition
+window.addEventListener('resize', () => {
+    const detailsElement = document.querySelector('.dropdown-card');
+    if (window.matchMedia('(min-width: 992px)').matches) {
+      // Si la pantalla es 'lg' o más grande
+      detailsElement.setAttribute('open', '');
+    } else {
+      // Si la pantalla es menor a 'lg'
+      detailsElement.removeAttribute('open');
+    }
+  });
