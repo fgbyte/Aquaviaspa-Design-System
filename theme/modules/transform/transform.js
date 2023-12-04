60fc2d2ce2ad634d010affd132024fb98701c8ba
guillezdev <guillezdev@gmail.com>
2023-12-04 17:03:16 -0500

    new module card-transform

---
+++ theme/modules/transform/transform.js
@@ -0,0 +1,17 @@
+const cardTransform = document.querySelector("#card-transform")
+const cardTransformRight = document.querySelector(".card-transform-right")
+
+document.addEventListener('DOMContentLoaded', () => {
+  cardTransformRight.addEventListener('mouseover', (e) => {
+    if (window.innerWidth > 993) {
+      cardTransform.classList.add('opac')
+    }
+  })
+  cardTransformRight.addEventListener('mouseout', (e) => {
+    cardTransform.classList.remove('opac')
+  })
+})
+
+
+
+
