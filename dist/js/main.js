60c03e3350985e2ec20e286700badc9f9605169d
fgbyte <fer.garciabayon@gmail.com>
2023-12-05 13:15:22 -0500

    fix ts errors

--- dist/js/main.js
+++ dist/js/main.js
@@ -17,12 +17,18 @@ window.addEventListener('resize', () => {
 const cardTransform = document.querySelector("#card-transform");
 const cardTransformRight = document.querySelector(".card-transform-right");
 document.addEventListener('DOMContentLoaded', () => {
-    cardTransformRight.addEventListener('mouseover', (e) => {
-        if (window.innerWidth > 993) {
-            cardTransform.classList.add('opac');
-        }
-    });
-    cardTransformRight.addEventListener('mouseout', (e) => {
-        cardTransform.classList.remove('opac');
-    });
+    if (cardTransformRight) {
+        cardTransformRight.addEventListener('mouseover', (e) => {
+            if (window.innerWidth > 993) {
+                if (cardTransform) {
+                    cardTransform.classList.add('opac');
+                }
+            }
+        });
+        cardTransformRight.addEventListener('mouseout', (e) => {
+            if (cardTransform) {
+                cardTransform.classList.remove('opac');
+            }
+        });
+    }
 });
--- theme/modules/transform/transform.ts
+++ theme/modules/transform/transform.ts
@@ -3,14 +3,20 @@ const cardTransform = document.querySelector("#card-transform")
 const cardTransformRight = document.querySelector(".card-transform-right")
 
 document.addEventListener('DOMContentLoaded', () => {
-  cardTransformRight.addEventListener('mouseover', (e) => {
-    if (window.innerWidth > 993) {
-      cardTransform.classList.add('opac')
-    }
-  })
-  cardTransformRight.addEventListener('mouseout', (e) => {
-    cardTransform.classList.remove('opac')
-  })
+  if (cardTransformRight) {
+    cardTransformRight.addEventListener('mouseover', (e) => {
+      if (window.innerWidth > 993) {
+        if (cardTransform) {
+          cardTransform.classList.add('opac')
+        }
+      }
+    })
+    cardTransformRight.addEventListener('mouseout', (e) => {
+      if (cardTransform) {
+        cardTransform.classList.remove('opac')
+      }
+    })
+  }
 })
 
 
