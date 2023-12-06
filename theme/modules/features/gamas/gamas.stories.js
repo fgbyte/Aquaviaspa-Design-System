22dc7423790ad81a7e6ea55b10b9a9146aa0c8f0
fgbyte <fer.garciabayon@gmail.com>
2023-12-06 12:37:32 -0500

    gamas module done[update]

---
+++ theme/modules/features/gamas/gamas.stories.js
@@ -0,0 +1,23 @@
+export const gamas = `
+<section class="gamas">
+<div class="gamas__text">
+    <h2>Una amplia garantía</h2>
+    <p>Ofrecemos garantías de larga duración, ya que la calidad y fiabilidad de nuestros spas nos lo permite.
+    </p>
+</div>
+<div class="gamas__cards">
+    <div class="card-gama">
+        <img src="/dist/assets/icons/Logo.svg" alt="">
+        <h4>15 años estructura</h4>
+    </div>
+    <div class="card-gama">
+        <img src="/dist/assets/icons/Logo.svg" alt="">
+        <h4>10 años acrílico</h4>
+    </div>
+    <div class="card-gama">
+        <img src="/dist/assets/icons/Logo.svg" alt="">
+        <h4>2 años componentes</h4>
+    </div>
+</div>
+</section>
+`
