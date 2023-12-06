124ae9d5448ca5055870cc74c5c541ce3cebee5d
fgbyte <fer.garciabayon@gmail.com>
2023-12-06 15:56:03 -0500

    update links stories

--- theme/components/links/links.stories.js
+++ theme/components/links/links.stories.js
@@ -20,17 +20,9 @@ Link menu arrow upper
 <span><img src="/dist/assets/icons/link-menu-arrow.svg" alt=""></span>
 </a>`
 
-export const link7 = `
-<a class="link-menu-arrow  link-menu-arrow--lang" href="#">
-        <img src="/dist/assets/icons/link-menu-arrow--lang.svg"></img>España
-        <span><img src="/dist/assets/icons/link-menu-arrow--lang-arrow.svg" alt=""></span>
-    </a>
-`
-
 export const link8 = `
 <a  class="link-arrow-grow" href="#">
 <span>Ir al Blog</span>
-
 <div class="d-flex align-items-center container-arrow">
     <div class="link__arrow"></div>
     <svg xmlns="http://www.w3.org/2000/svg" width="7.026" height="20.162" viewBox="0 0 9.026 20.162">
@@ -41,7 +33,6 @@ export const link8 = `
           stroke-width="1" />
       </g>
     </svg>
-
   </div>
 </a>
 `
@@ -49,7 +40,6 @@ export const link8 = `
 export const link9 = `
 <a  class="link-arrow-grow-white" href="#">
 <span>Descarga nuestro catalogo</span>
-
 <div class="d-flex align-items-center container-arrow-white">
     <div class="link__arrow-white"></div>
     <svg xmlns="http://www.w3.org/2000/svg" width="7.026" height="20.162" viewBox="0 0 9.026 20.162">
@@ -60,7 +50,25 @@ export const link9 = `
           stroke-width="1" />
       </g>
     </svg>
-
   </div>
+</a>
+`
+
+
+export const link10 = `
+<a class="link-arrow-grow-white-reverse" href="#">
+<div class="d-flex align-items-center container-arrow-white">
+  <div class="link__arrow-white"></div>
+  <svg xmlns="http://www.w3.org/2000/svg" width="7.026" height="20.162" viewBox="0 0 9.026 20.162">
+    <g id="Grupo_12494" class="link__arrow-svg-white" data-name="Grupo 12494"
+      transform="translate(-326.008 -160.675)">
+      <line id="Línea_18" data-name="Línea 18" x1="8.24" y1="10.486" transform="translate(326.401 160.984)"
+        stroke-width="1" />
+      <line id="Línea_19" data-name="Línea 19" y1="9.029" x2="8.24" transform="translate(326.401 171.471)"
+        stroke-width="1" />
+    </g>
+  </svg>
+</div>
+<span>Aurora</span>
 </a>
 `
