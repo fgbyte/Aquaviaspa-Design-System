dad30c6d6cc99f8063fab6358063b7371cf74219
fgbyte <fer.garciabayon@gmail.com>
2023-12-09 03:04:29 -0500

    update stories

---
+++ theme/modules/features/intro/intro.stories.js
@@ -0,0 +1,21 @@
+export const intro = `
+<section class="intro">
+<div class="intro__container">
+    <div class="intro__container__text">
+        <blockquote>Aquavia Spa, <span>líder europeo en la fabricación de spas y swimspas</span></blockquote>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+            et dolore
+            magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea
+            commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt
+            ut labore et dolore magna aliqua ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+            tempor
+            incididunt ut labore et dolore magna aliqua.</p>
+    </div>
+    <div class="intro__container__img">
+        <img src="/dist/assets/img/intro-home.png" alt="Made in Europe">
+    </div>
+</div>
+</section>
+`
