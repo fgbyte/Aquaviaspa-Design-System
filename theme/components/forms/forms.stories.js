df278b4d6186b03d5c383ac83e3ba4f2d92da3be
fgbyte <fer.garciabayon@gmail.com>
2023-12-09 03:26:10 -0500

    add forms tos stories

--- theme/components/forms/forms.stories.js
+++ theme/components/forms/forms.stories.js
@@ -1,87 +1,76 @@
-export const card1 = `
-<a href="#" class="card-category-blog gb-card">
-<div class="card-category-blog__bg">
-</div>
-
-<img class="card-category-blog__plus" src="/dist/assets/icons/bx-plus.svg" alt="plus">
-
-<h4>Descubre los beneficios de la hidroterapia en tu spa</h4>
-<p>La técnica terapéutica de la hidroterapia, tiene muchos beneficios que han sido utilizados para tratar una
-  variedad de afecciones y con el objetivo de mejorar…</p>
-</a>
+export const searchContainer = `
+<search class="search-container">
+<input type="search" id="searchInput" name="searchInput" placeholder="Buscar por palabras">
+<img width="22" src="/dist/assets/icons/search.svg" alt="search icon">
+</search>
 `
 
-
-export const card2 = `
-<a href="#" class="card-category-blog-lite gb-card">
-<div class="card-category-blog-lite__container">
-  <div class="card-category-blog-lite__bg">
-    <img class="card-category-blog-lite__plus" src="/dist/assets/icons/bx-plus.svg" alt="plus">
-  </div>
-</div>
-
-<h4>Descubre los beneficios de la hidroterapia en tu spa</h4>
-<p>La técnica terapéutica de la hidroterapia, tiene muchos beneficios que han sido utilizados para tratar una
-  variedad de afecciones y con el objetivo de mejorar…</p>
-</a>
+export const searchContainerWhite = `
+<search class="search-container-withe">
+<input type="search" id="searchInputMaps" name="searchInputMaps" placeholder="Introduzca una ubicación">
+<img width="22" src="/dist/assets/icons/search.svg" alt="search icon">
+</search>
 `
 
-
-export const card3 = `
-<button class="card-map">
-
-<img class="card-map__img" src="/dist/assets/icons/location-dark.svg" alt="location icon">
-
-<div class="card-map__description">
-  <h3 class="">Catalonia AquaGrup <span>(Spain)</span></h3>
-  <h4>Av de la Porta Diagonal, 34. Cornellà de Llobregat (Salida 15 Rda. de Dalt).</h4>
-  <span class="card-map__description--span">ShowRoom</span>
-  <p>T. 933 770 665</p>
-  <p>nfo@cataloniaaqua.com</p>
-  <div class="d-flex align-items-center justify-content-between gap-5">
-    <p class="fw-bold">www.cataloniaaqua.com</p>
-    <span class="d-flex align-items-center">85km <img class="ml-auto" width="16px"
-        src="/dist/assets/icons/Shape.svg" alt="Shape"></span>
-  </div>
+export const formBasic = `
+<form class="form-full bg-light px-5 py-5">
+<div class="form-full__container  ">
+  <label class="d-flex flex-column w-100" for="">
+    <span>Nombre*</span>
+    <input type="text">
+  </label>
+  <label class="d-flex flex-column w-100" for="">
+    <span> Apellidos*</span>
+    <input type="text">
+  </label>
 </div>
-</button>
-`
-
-
-export const card4 = `
-<div class="card-menu-filters">
-<div class="card-menu-filters__img">
+<div class="form-full__container  ">
+  <label class="d-flex flex-column w-100" for="">
+    <span> Teéfono*</span>
+    <input type="text">
+  </label>
+  <label class="d-flex flex-column w-100" for="">
+    <span> Correo elecrónico*</span>
+    <input type="text">
+  </label>
 </div>
-<span class="card-menu-filters__span">2-3</span>
-</div>
-`
-
-
-export const card5 = `
-<div class="card-transform">
-
-<div class="card-transform__card-inside">
-  <img class="card-transform__card-inside--img" src="/dist/assets/icons/Logo.svg" alt="Logo">
-  <h3>Spas</h3>
-  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt labore.</p>
-  <a class="link-arrow-grow-white" href="#">
-    <span>Descubre nuestros spas</span>
-
-    <div class="d-flex align-items-center container-arrow-white">
-      <div class="link__arrow-white"></div>
-      <svg xmlns="http://www.w3.org/2000/svg" width="7.026" height="20.162" viewBox="0 0 9.026 20.162">
-        <g id="Grupo_12494" class="link__arrow-svg-white" data-name="Grupo 12494"
-          transform="translate(-326.008 -160.675)">
-          <line id="Línea_18" data-name="Línea 18" x1="8.24" y1="10.486" transform="translate(326.401 160.984)"
-            stroke-width="1" />
-          <line id="Línea_19" data-name="Línea 19" y1="9.029" x2="8.24" transform="translate(326.401 171.471)"
-            stroke-width="1" />
-        </g>
-      </svg>
-
-    </div>
-  </a>
+<div class="form-full__container  ">
+  <label class="d-flex flex-column w-100" for="">
+    <span> Códigopostal*</span>
+    <input type="text">
+  </label>
+  <label class="d-flex flex-column w-100" for="">
+    <span> Polación*</span>
+    <input type="text">
+  </label>
 </div>
 
-</div>
+<label class="d-flex flex-column w-100">
+  <span>País*</span>
+  <select class="form-full__select" id="opciones" name="opciones">
+    <option value="opcion1">– Por favor, elija una opción –</option>
+    <option value="opcion2">– Por favor, elija una opción –</option>
+    <option value="opcion3">– Por favor, elija una opción –</option>
+  </select>
+</label>
+
+<label class="d-flex flex-column w-100" for="comentario">
+  <span>Mensaje*</span>
+  <textarea class="form-full__textarea" id="comentario" name="comentario"></textarea>
+</label>
+
+<label class="form-full__checkbox">
+
+  <p><input type="checkbox" id="miCheckbox" name="miCheckbox">Al enviar el formulario, acepto que estoy
+    de acuerdo con los <b>términos y condiciones.</b></p>
+</label>
+
+<div class="mx-auto text-center form-full__terms">Este sitio está protegido por reCAPTCHA y se aplican la <a
+    href="#"><b>política
+      de privacidad</b></a> y los <a href="#"><b>términos de servicio</b></a> de Google.</div>
+
+
+<button type="button" class="btn-ficha">Enviar</button>
+
+</form>
 `
