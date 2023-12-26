79351839d433cfd2cce1b4fa13b0dc349245f9ef
fgbyte <fer.garciabayon@gmail.com>
2023-12-26 13:07:15 -0500

    fix bg cards-2

--- theme/components/cards-2/cards-2.stories.js
+++ theme/components/cards-2/cards-2.stories.js
@@ -1,6 +1,7 @@
+//card-category-blog--full/Home
 export const card1 = `
 <a href="#" class="card-category-blog gb-card">
-<div class="card-category-blog__bg">
+<div style="background-image: url('/dist/assets/img/card-beneficios1.webp');" class="card-category-blog__bg">
 </div>
 
 <img class="card-category-blog__plus" src="/dist/assets/icons/bx-plus.svg" alt="plus">
@@ -11,11 +12,11 @@ export const card1 = `
 </a>
 `
 
-
+//card-category-blog--lite/Blog
 export const card2 = `
 <a href="#" class="card-category-blog-lite gb-card">
 <div class="card-category-blog-lite__container">
-  <div class="card-category-blog-lite__bg">
+  <div style="background-image: url('/dist/assets/img/card-beneficios1.webp');" class="card-category-blog-lite__bg">
     <img class="card-category-blog-lite__plus" src="/dist/assets/icons/bx-plus.svg" alt="plus">
   </div>
 </div>
@@ -26,62 +27,62 @@ export const card2 = `
 </a>
 `
 
-
+//card-map/Donde Comprar
 export const card3 = `
 <button class="card-map">
 
 <img class="card-map__img" src="/dist/assets/icons/location-dark.svg" alt="location icon">
 
 <div class="card-map__description">
-  <h3 class="">Catalonia AquaGrup <span>(Spain)</span></h3>
+  <h3>Catalonia AquaGrup <span>(Spain)</span></h3>
   <h4>Av de la Porta Diagonal, 34. Cornellà de Llobregat (Salida 15 Rda. de Dalt).</h4>
-  <span class="card-map__description--span">ShowRoom</span>
+  <div class="card-map__description--span">ShowRoom</div>
   <p>T. 933 770 665</p>
   <p>nfo@cataloniaaqua.com</p>
   <div class="d-flex align-items-center justify-content-between gap-5">
-    <p class="fw-bold">www.cataloniaaqua.com</p>
-    <span class="d-flex align-items-center">85km <img class="ml-auto" width="16px"
-        src="/dist/assets/icons/Shape.svg" alt="Shape"></span>
+    <div class="fw-bold">www.cataloniaaqua.com</div>
+    <div class="d-flex align-items-center">85km <img class="ml-auto" width="16px"
+        src="/dist/assets/icons/Shape.svg" alt="Shape"></div>
   </div>
 </div>
 </button>
 `
 
-
+//card-menu--filters/Menu Lateral
 export const card4 = `
 <div class="card-menu-filters">
-<div class="card-menu-filters__img">
-</div>
-<span class="card-menu-filters__span">2-3</span>
-</div>
+    <div style="background-image: url('/dist/assets/img/card-beneficios1.webp');" class="card-menu-filters__img">
+    </div>
+    <div class="card-menu-filters__span">2-3</div>
+  </div>
 `
 
-
+//card-transform--lg/Home
 export const card5 = `
-<div class="card-transform">
+<div style="background-image: url('/dist/assets/img/card-transform.webp');" class="card-transform">
 
-<div class="card-transform__card-inside">
-  <img class="card-transform__card-inside--img" src="/dist/assets/icons/Logo.svg" alt="Logo">
-  <h3>Spas</h3>
-  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt labore.</p>
-  <a class="link-arrow-grow-white" href="#">
-    <span>Descubre nuestros spas</span>
+    <div class="card-transform__card-inside">
+      <img class="card-transform__card-inside--img" src="/dist/assets/icons/Logo.svg" alt="Logo">
+      <h3>Spas</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt labore.</p>
+      <a class="link-arrow-grow-white" href="#">
+        <span>Descubre nuestros spas</span>
 
-    <div class="d-flex align-items-center container-arrow-white">
-      <div class="link__arrow-white"></div>
-      <svg xmlns="http://www.w3.org/2000/svg" width="7.026" height="20.162" viewBox="0 0 9.026 20.162">
-        <g id="Grupo_12494" class="link__arrow-svg-white" data-name="Grupo 12494"
-          transform="translate(-326.008 -160.675)">
-          <line id="Línea_18" data-name="Línea 18" x1="8.24" y1="10.486" transform="translate(326.401 160.984)"
-            stroke-width="1" />
-          <line id="Línea_19" data-name="Línea 19" y1="9.029" x2="8.24" transform="translate(326.401 171.471)"
-            stroke-width="1" />
-        </g>
-      </svg>
+        <div class="d-flex align-items-center container-arrow-white">
+          <div class="link__arrow-white"></div>
+          <svg xmlns="http://www.w3.org/2000/svg" width="7.026" height="20.162" viewBox="0 0 9.026 20.162">
+            <g id="Grupo_12494" class="link__arrow-svg-white" data-name="Grupo 12494"
+              transform="translate(-326.008 -160.675)">
+              <line id="Línea_18" data-name="Línea 18" x1="8.24" y1="10.486" transform="translate(326.401 160.984)"
+                stroke-width="1" />
+              <line id="Línea_19" data-name="Línea 19" y1="9.029" x2="8.24" transform="translate(326.401 171.471)"
+                stroke-width="1" />
+            </g>
+          </svg>
 
+        </div>
+      </a>
     </div>
-  </a>
-</div>
 
-</div>
+  </div>
 `
--- theme/modules/features/transform/transform.stories.js
+++ theme/modules/features/transform/transform.stories.js
@@ -1,58 +1,49 @@
 export const transform1 = `
 <section class="d-lg-flex gap-1 position-relative">
-
-
-<article id="card-transform" class="card-transform">
-
-  <div class="card-transform__card-inside">
-    <img class="card-transform__card-inside--img" src="/dist/assets/icons/Logo.svg" alt="Logo">
-    <h3>Spas</h3>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt labore.</p>
-    <a class="link-arrow-grow-white" href="#">
-      <span>Descubre nuestros spas</span>
-
-      <div class="d-flex align-items-center container-arrow-white">
-        <div class="link__arrow-white"></div>
-        <svg xmlns="http://www.w3.org/2000/svg" width="7.026" height="20.162" viewBox="0 0 9.026 20.162">
-          <g id="Grupo_12494" class="link__arrow-svg-white" data-name="Grupo 12494"
-            transform="translate(-326.008 -160.675)">
-            <line id="Línea_18" data-name="Línea 18" x1="8.24" y1="10.486" transform="translate(326.401 160.984)"
-              stroke-width="1" />
-            <line id="Línea_19" data-name="Línea 19" y1="9.029" x2="8.24" transform="translate(326.401 171.471)"
-              stroke-width="1" />
-          </g>
-        </svg>
-
+    <article id="card-transform" class="card-transform" style="background-image: url('/dist/assets/img/card-transform.webp');">
+      <div class="card-transform__card-inside">
+        <img class="card-transform__card-inside--img" src="/dist/assets/icons/Logo.svg" alt="Logo">
+        <h3>Spas</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt labore.</p>
+        <a class="link-arrow-grow-white" href="#">
+          <span>Descubre nuestros spas</span>
+          <div class="d-flex align-items-center container-arrow-white">
+            <div class="link__arrow-white"></div>
+            <svg xmlns="http://www.w3.org/2000/svg" width="7.026" height="20.162" viewBox="0 0 9.026 20.162">
+              <g id="Grupo_12494" class="link__arrow-svg-white" data-name="Grupo 12494"
+                transform="translate(-326.008 -160.675)">
+                <line id="Línea_18" data-name="Línea 18" x1="8.24" y1="10.486" transform="translate(326.401 160.984)"
+                  stroke-width="1" />
+                <line id="Línea_19" data-name="Línea 19" y1="9.029" x2="8.24" transform="translate(326.401 171.471)"
+                  stroke-width="1" />
+              </g>
+            </svg>
+          </div>
+        </a>
       </div>
-    </a>
-  </div>
+    </article>
 
-</article>
-
-<article class="card-transform-right">
-
-  <div class="card-transform__card-inside">
-    <img class="card-transform__card-inside--img" src="/dist/assets/icons/Logo.svg" alt="Logo">
-    <h3>Swimspas</h3>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt labore.</p>
-    <a class="link-arrow-grow-white" href="#">
-      <span>Descúbra nuestros swimspas</span>
-
-      <div class="d-flex align-items-center container-arrow-white">
-        <div class="link__arrow-white"></div>
-        <svg xmlns="http://www.w3.org/2000/svg" width="7.026" height="20.162" viewBox="0 0 9.026 20.162">
-          <g id="Grupo_12494" class="link__arrow-svg-white" data-name="Grupo 12494"
-            transform="translate(-326.008 -160.675)">
-            <line id="Línea_18" data-name="Línea 18" x1="8.24" y1="10.486" transform="translate(326.401 160.984)"
-              stroke-width="1" />
-            <line id="Línea_19" data-name="Línea 19" y1="9.029" x2="8.24" transform="translate(326.401 171.471)"
-              stroke-width="1" />
-          </g>
-        </svg>
-
+    <article class="card-transform-right" style="background-image: url('/dist/assets/img/card-transform-rigth2.webp');">
+      <div class="card-transform__card-inside">
+        <img class="card-transform__card-inside--img" src="/dist/assets/icons/Logo.svg" alt="Logo">
+        <h3>Swimspas</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt labore.</p>
+        <a class="link-arrow-grow-white" href="#">
+          <span>Descúbra nuestros swimspas</span>
+          <div class="d-flex align-items-center container-arrow-white">
+            <div class="link__arrow-white"></div>
+            <svg xmlns="http://www.w3.org/2000/svg" width="7.026" height="20.162" viewBox="0 0 9.026 20.162">
+              <g id="Grupo_12494" class="link__arrow-svg-white" data-name="Grupo 12494"
+                transform="translate(-326.008 -160.675)">
+                <line id="Línea_18" data-name="Línea 18" x1="8.24" y1="10.486" transform="translate(326.401 160.984)"
+                  stroke-width="1" />
+                <line id="Línea_19" data-name="Línea 19" y1="9.029" x2="8.24" transform="translate(326.401 171.471)"
+                  stroke-width="1" />
+              </g>
+            </svg>
+          </div>
+        </a>
       </div>
-    </a>
-  </div>
-</article>
-</section>
+    </article>
+  </section>
 `
