07f390a77ff84f5f5e56ce4853711e16049b5d08
fgbyte <fer.garciabayon@gmail.com>
2023-12-27 15:31:01 -0500

    fix null validations

--- dist/js/main.js
+++ dist/js/main.js
@@ -11,23 +11,39 @@ if (window.matchMedia('(min-width: 992px)').matches) {
 const cardTransform = document.querySelector("#card-transform");
 const cardTransformRight = document.querySelector(".card-transform-right");
 document.addEventListener('DOMContentLoaded', () => {
-    cardTransformRight.addEventListener('mouseover', (e) => {
-        if (window.innerWidth > 993) {
-            cardTransform.classList.add('opac');
-        }
-    });
-    cardTransformRight.addEventListener('mouseout', (e) => {
-        cardTransform.classList.remove('opac');
-    });
+    if (cardTransformRight) {
+        cardTransformRight.addEventListener('mouseover', (e) => {
+            if (window.innerWidth > 993) {
+                if (cardTransform) {
+                    cardTransform.classList.add('opac');
+                }
+            }
+        });
+        cardTransformRight.addEventListener('mouseout', (e) => {
+            if (cardTransform) {
+                cardTransform.classList.remove('opac');
+            }
+        });
+    }
 });
 //Valores module
 const tabsValores = Array.from({ length: 5 }, (_, i) => document.getElementById(`valor-${i + 1}`));
 const modalsValores = Array.from({ length: 5 }, (_, i) => document.getElementById(`modal-valor-${i + 1}`));
 //ocultar los modales al principio menos el 1
-modalsValores.forEach((modal, i) => modal.style.display = i === 0 ? 'block' : 'none');
+modalsValores.forEach((modal, i) => {
+    if (modal) {
+        modal.style.display = i === 0 ? 'block' : 'none';
+    }
+});
 //agregar eventListener a las tabsValores
 tabsValores.forEach((tab, i) => {
-    tab.addEventListener('click', () => {
-        modalsValores.forEach((modal, j) => modal.style.display = i === j ? 'block' : 'none');
-    });
+    if (tab) {
+        tab.addEventListener('click', () => {
+            modalsValores.forEach((modal, j) => {
+                if (modal) {
+                    modal.style.display = i === j ? 'block' : 'none';
+                }
+            });
+        });
+    }
 });
--- theme/modules/features-2/valores/valores.ts
+++ theme/modules/features-2/valores/valores.ts
@@ -3,11 +3,21 @@ const tabsValores = Array.from({ length: 5 }, (_, i) => document.getElementById(
 const modalsValores = Array.from({ length: 5 }, (_, i) => document.getElementById(`modal-valor-${i+1}`));
 
 //ocultar los modales al principio menos el 1
-modalsValores.forEach((modal, i) => modal!.style.display = i === 0 ? 'block' : 'none');
+modalsValores.forEach((modal, i) => {
+  if(modal){
+    modal.style.display = i === 0 ? 'block' : 'none';
+  }
+});
 
 //agregar eventListener a las tabsValores
 tabsValores.forEach((tab, i) => {
-    tab!.addEventListener('click', () => {
-      modalsValores.forEach((modal, j) => modal!.style.display = i === j ? 'block' : 'none');
-    });
+    if(tab){
+        tab.addEventListener('click', () => {
+          modalsValores.forEach((modal, j) => {
+            if(modal){
+                modal.style.display = i === j ? 'block' : 'none';
+            }
+          });
+        });
+    }
 });
