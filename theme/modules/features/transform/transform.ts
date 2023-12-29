97f8fa5a4daff57800ffa9186ce9256285daf3d8
guillezdev <guillezdev@gmail.com>
2023-12-28 19:00:46 -0500

    transform module done

--- theme/modules/features/transform/transform.ts
+++ theme/modules/features/transform/transform.ts
@@ -1,20 +1,21 @@
 //transform module
 const cardTransform = document.querySelector("#card-transform")
 const cardTransformRight = document.querySelector(".card-transform-right")
+const transformModule = document.querySelector(".transform-module")
 
 document.addEventListener('DOMContentLoaded', () => {
-    if (cardTransformRight) {
-        cardTransformRight.addEventListener('mouseover', (e) => {
-          if (window.innerWidth > 993) {
-              if (cardTransform) {
-                  cardTransform.classList.add('opac')
-              }
-          }
-        })
-        cardTransformRight.addEventListener('mouseout', (e) => {
-            if (cardTransform) {
-                cardTransform.classList.remove('opac')
-            }
-        })
-    }
+	if (cardTransformRight) {
+		cardTransformRight.addEventListener('mouseover', (e) => {
+			if (window.innerWidth > 993) {
+				if (cardTransform) {
+					cardTransform?.classList.add('opac')
+				}
+			}
+		})
+		cardTransformRight.addEventListener('mouseout', (e) => {
+			if (cardTransform) {
+				cardTransform?.classList.remove('opac')
+			}
+		})
+	}
 })
