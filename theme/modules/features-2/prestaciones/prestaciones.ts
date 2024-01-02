a70222b2bc630e9cee35d93a0fbdb90a1530379b
fgbyte <fer.garciabayon@gmail.com>
2024-01-02 13:34:43 -0500

    working on prestaciones slider

--- theme/modules/features-2/prestaciones/prestaciones.ts
+++ theme/modules/features-2/prestaciones/prestaciones.ts
@@ -1,52 +1,63 @@
-//prestaciones inner
+//prestaciones inner generic para todos las categories
 document.addEventListener('DOMContentLoaded', () => {
     //pr-#
-    const links = Array.from({ length: 3 }, (_, i) => document.getElementById(`pr-${i + 1}`));
+    const links = Array.from({ length: 3 }, (_, i) => document.querySelectorAll(`#pr-${i + 1}`));
     //prest-#
-    const prests = Array.from({ length: 3 }, (_, i) => document.getElementById(`prest-${i + 1}`));
+    const prests = Array.from({ length: 3 }, (_, i) => document.querySelectorAll(`#prest-${i + 1}`));
+
+    // console.log(links);
+    // console.log(prests);
 
     //ocultar los modales al principio menos el 1
     //! tiene que ser solo en LG
-    if (window.matchMedia('(min-width: 992px)').matches){
+    if (window.matchMedia('(min-width: 992px)').matches) {
         prests.forEach((pre, i) => {
-            if (pre) {
-                pre.style.display = i === 0
-                    ? 'block'
-                    : 'none';
-            }
-        })
+            // console.log(pre)
+            // console.log(i)
+            pre.forEach((element) => {
+                if (element instanceof HTMLElement) {
+                    element.style.display = i === 0 ? 'block' : 'none';//el 1ro del array
+                }
+            });
+        });
     }
     window.addEventListener('resize', () => {
-        if (window.matchMedia('(min-width: 992px)').matches){
+        if (window.matchMedia('(min-width: 992px)').matches) {
             prests.forEach((pre, i) => {
-                if (pre) {
-                    pre.style.display = i === 0
-                        ? 'block'
-                        : 'none';
-                }
+                pre.forEach(element => {
+                    if (element instanceof HTMLElement) {
+                        element.style.display = i === 0
+                            ? 'block'
+                            : 'none';
+                    }
+                });
             })
         } else {
             prests.forEach((pre) => {
-                if (pre) {
-                    pre.style.display = 'block';
-                }
+                pre.forEach(element => {
+                    if (element instanceof HTMLElement) {
+                        element.style.display = 'block';
+                    }
+                });
             })
         }
     });
 
     links.forEach((link, i) => {
-        if (link) {
-            link.addEventListener('click', () => {
+        link.forEach((singleLink, _) => {
+            singleLink.addEventListener('click', () => {
                 prests.forEach((prest, j) => {
                     if (prest) {
-                        prest.style.display = i === j
-                            ? 'block'
-                            : 'none';
+                        prest.forEach((singlePrest, __) => {
+                            if (singlePrest instanceof HTMLElement) {
+                                singlePrest.style.display = i === j
+                                    ? 'block'
+                                    : 'none';
+                            }
+                        });
                     }
                 })
             })
-        }
-    })
+        });
+    });
 })
-
-//prestaciones categories
