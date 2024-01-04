6cfe0bfd3e3f6c0a862d53033ef4a80025d17e30
fgbyte <fer.garciabayon@gmail.com>
2024-01-04 14:24:37 -0500

    prestaciones done

--- theme/modules/features-2/prestaciones/prestaciones.ts
+++ theme/modules/features-2/prestaciones/prestaciones.ts
@@ -3,11 +3,13 @@ document.addEventListener('DOMContentLoaded', () => {
 const filterLinks = Array.from({ length: 5 }, (_, i) => document.querySelectorAll(`#fil-${i + 1}`));
 const categories = Array.from({ length: 5 }, (_, i) => document.querySelectorAll(`#category-${i + 1}`));
 
-
+//links logic
 filterLinks.forEach((nodeLink) => {
     nodeLink.forEach((link) => {
         link.addEventListener('click', (event) => {
-            showOnly(0);//muestra la card 1 en todas
+            if (window.matchMedia(`(min-width: ${special}px)`).matches) {
+                showOnly(0)//el 1ro del array
+            }
             // Remove 'checked' attribute from all filter links
             filterLinks.forEach((otherLink) => {
                 otherLink.forEach((element) => {
@@ -21,6 +23,37 @@ filterLinks.forEach((nodeLink) => {
         });
     });
  });
+
+ //only 1 first logic
+ categories.forEach((nodeCat, index) => {
+    nodeCat.forEach((singleCat) => {
+        if (singleCat instanceof HTMLElement) {
+            singleCat.style.display = index === 0
+                ? 'flex'
+                : 'none';
+        }
+    })
+ })
+
+ //render logic
+ filterLinks.forEach((nodeLink, i) => {
+     nodeLink.forEach((singleLink, _) => {
+        singleLink.addEventListener('click', () => {
+            categories.forEach((nodeCat, j) => {
+                if (nodeCat) {
+                    nodeCat.forEach((singleCat, __) => {
+                        if (singleCat instanceof HTMLElement) {
+                            singleCat.style.display = i === j
+                                ? 'flex'
+                                : 'none';
+                        }
+                    })
+                }
+            })
+        })
+     })
+ })
 })
 
 //TODO: Renderizado condicional para las categories
+
