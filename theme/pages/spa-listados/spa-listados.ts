571abf96b7b96f6205dea870bc59867937f3c234
fgbyte <fer.garciabayon@gmail.com>
2024-01-16 16:42:17 -0500

    loadmore done

--- theme/pages/spa-listados/spa-listados.ts
+++ theme/pages/spa-listados/spa-listados.ts
@@ -1,3 +1,6 @@
+const mobileMediaQuery = window.matchMedia('(max-width: 768px)');
+const desktopMediaQuery = window.matchMedia('(min-width: 768px)');
+
 document.addEventListener('DOMContentLoaded', function () {
   const cards = document.querySelectorAll('.card-spa');
   const loadMoreButton = document.getElementById('loadMore') as HTMLElement;
@@ -5,7 +8,7 @@ document.addEventListener('DOMContentLoaded', function () {
   function showHiddenCards() {
     cards.forEach((card, index) => {
       if (index >= 6) {
-        card.classList.toggle('hidden-card');
+        card.classList.remove('hidden-card');
       }
     });
   }
@@ -13,9 +16,30 @@ document.addEventListener('DOMContentLoaded', function () {
   function hideLoadMore() {
     loadMoreButton.style.display = 'none';
   }
+  function showLoadMore() {
+    loadMoreButton.style.display = 'flex';
+  }
 
   loadMoreButton.addEventListener('click', showHiddenCards);
   loadMoreButton.addEventListener('click', hideLoadMore);
+
+
+  mobileMediaQuery.addEventListener('change', (event) => {
+    if (event.matches) {
+      cards.forEach((card, index) => {
+        showLoadMore()
+        if (index >= 6) {
+          card.classList.add('hidden-card');
+        }
+      });
+    }
+  })
+
+  desktopMediaQuery.addEventListener('change', (event) => {
+    if (event.matches) {
+      showHiddenCards()
+    }
+  })
 
   if (window.innerWidth <= 768) {
     cards.forEach((card, index) => {
