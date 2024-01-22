436e06e9e90ec14989440d55beabdba2062e2180
guillezdev <guillezdev@gmail.com>
2024-01-22 18:04:57 -0500

    some changes

--- dist/js/main.js
+++ dist/js/main.js
@@ -641,20 +641,15 @@ document.addEventListener('DOMContentLoaded', function () {
         return bannerHeight - navbarHeight;
     }
 });
-// Obtiene referencias a los formularios
 var formContacto = document.getElementById('form-contacto');
 var formDistribuidor = document.getElementById('form-distribuidor');
-// Obtiene referencias a los enlaces de filtro
 var linkContacto = document.getElementById('fil-1');
 var linkDistribuidor = document.getElementById('fil-2');
-// Escucha los clics en los enlaces de filtro
 linkContacto.addEventListener('click', function () {
-    // Muestra el formulario de contacto y oculta el de distribuidor
     formContacto.style.display = 'flex';
     formDistribuidor.style.display = 'none';
 });
 linkDistribuidor.addEventListener('click', function () {
-    // Muestra el formulario de distribuidor y oculta el de contacto
     formContacto.style.display = 'none';
     formDistribuidor.style.display = 'flex';
 });
