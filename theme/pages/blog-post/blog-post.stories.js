d269c5ff7bd9a4477b3b46baf64f85a1d39c0fe4
fgbyte <fer.garciabayon@gmail.com>
2024-01-25 16:59:39 -0500

    update stories

---
+++ theme/pages/blog-post/blog-post.stories.js
@@ -0,0 +1,170 @@
+export const blogPost = `
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/dist/css/main.css">
+    <title>Blog Post</title>
+</head>
+
+<body class="posts-page">
+    <!-- *****Banner Post******* -->
+    <section id="hero" class="banner-posts banner-posts--blog">
+        <div class="banner-posts__img">
+            <img class="image-sm" src="/dist/assets/img/card-beneficios1.webp" alt="">
+            <img class="image-lg" src="/dist/assets/img/card-beneficios1.webp" alt="">
+        </div>
+    </section>
+    <!-- *****Intro Vertical******* -->
+    <section class="post-content">
+        <div class="intro-vertical">
+            <div class="intro-vertical__text">
+                <h2>Descubre los beneficios de la hidroterapia en tu spa</h2>
+                <p>La técnica terapéutica de la hidroterapia, tiene muchos beneficios que han sido utilizados para
+                    tratar
+                    una variedad de afecciones y con el objetivo de mejorar la salud y el bienestar de las personas.
+                    Esta
+                    técnica se basa en el uso del agua para estimular el cuerpo y promover la curación. En la
+                    actualidad, la
+                    hidroterapia se ha convertido en una forma popular de tratamiento que se puede hacer en casa
+                    mediante,
+                    los distintos tipos de spas que hay en el mercado.</p>
+
+                <p>La hidroterapia puede ser utilizada para tratar una amplia variedad de afecciones, desde dolores
+                    musculares y articulares hasta problemas de la piel y el sistema respiratorio. Una de las formas más
+                    populares de hidroterapia es la terapia de baño, en la que se sumerge al cuerpo en agua caliente o
+                    fría
+                    para aliviar el dolor y reducir la inflamación. Los spas, las saunas y los baños de vapor también
+                    son
+                    populares para la relajación y el tratamiento de ciertas dolencias.</p>
+            </div>
+        </div>
+
+        <article class="blog-article">
+            <img src="/dist/assets/img/imagen_simulada_detalle_noticia_blog_2@2x.webp" alt="">
+        </article>
+
+        <article class="blog-article">
+            <h3>Beneficios de la hidroterapia</h3>
+            <p>Además de sus beneficios terapéuticos, la hidroterapia también es conocida por sus beneficios relajantes.
+                La hidroterapia y otros tratamientos similares pueden proporcionar un ambiente tranquilo y sereno que
+                ayude a reducir el estrés y la ansiedad. Algunos de los beneficios terapéuticos más comunes de la
+                hidroterapia son:</p>
+            <p>Alivio del dolor: La hidroterapia puede ser especialmente efectiva para aliviar el dolor en músculos y
+                articulaciones. La hidroterapia puede proporcionar un ambiente relajante y terapéutico que ayuda a
+                reducir la inflamación y mejorar la movilidad.</p>
+
+            <p>Reducción del estrés: La relajación que proporciona el
+                agua caliente puede ayudar a aliviar la tensión física y emocional. Esto puede mejorar la calidad del
+                sueño y reducir los síntomas de la depresión y la ansiedad.</p>
+
+            <p>Fortalecimiento del sistema inmunológico: La
+                hidroterapia puede ayudar a fortalecer el sistema inmunológico. Al aumentar la circulación sanguínea y
+                reducir la inflamación, la hidroterapia puede ayudar al cuerpo a combatir las infecciones y
+                enfermedades.</p>
+
+            <p>Promoción de la curación: La hidroterapia puede ser una excelente manera de promover la
+                curación después de una lesión o cirugía. La hidroterapia ayuda a reducir la inflamación, mejora la
+                circulación sanguínea y proporciona un ambiente relajante que ayuda al cuerpo a sanar más rápidamente.
+            </p>
+            <p>En conclusión, la hidroterapia es una técnica terapéutica popular y efectiva que se ha utilizado durante
+                siglos para tratar una amplia variedad de afecciones. Los spas modernos ofrecen una amplia variedad de
+                tratamientos de hidroterapia para ayudar a los clientes a mejorar su salud y bienestar. Si estás
+                buscando una forma natural y no invasiva de mejorar tu salud y reducir el estrés, la hidroterapia es una
+                excelente opción para ti.</p>
+        </article>
+    </section>
+
+    <!-- *****Blog Slider******* -->
+    <section
+        class="prestaciones-slider prestaciones-slider--detalles mx-auto posts-page__slider section-slider-accsesorios">
+        <div class="section-slider-accsesorios__header">
+            <h3>OTROS POSTS QUE LE PUEDEN INTERESAR</h3>
+        </div>
+
+        <div id="category-1" class="prestaciones-slider__category">
+            <div class="prestaciones-slider__slider blog-post-slider">
+                <a href="#" class="card-category-blog-lite gb-card">
+                    <div class="card-category-blog-lite__container">
+                        <div style="background-image: url('/dist/assets/img/card-beneficios1.webp');"
+                            class="card-category-blog-lite__bg">
+                            <img class="card-category-blog-lite__plus" src="/dist/assets/icons/bx-plus.svg" alt="plus">
+                        </div>
+                    </div>
+ 
+                    <h4>Descubre los beneficios de la hidroterapia en tu spa</h4>
+                    <p>La técnica terapéutica de la hidroterapia, tiene muchos beneficios que han sido utilizados para
+                        tratar una
+                        variedad de afecciones y con el objetivo de mejorar…</p>
+                </a>
+                <a href="#" class="card-category-blog-lite gb-card">
+                    <div class="card-category-blog-lite__container">
+                        <div style="background-image: url('/dist/assets/img/card-beneficios1.webp');"
+                            class="card-category-blog-lite__bg">
+                            <img class="card-category-blog-lite__plus" src="/dist/assets/icons/bx-plus.svg" alt="plus">
+                        </div>
+                    </div>
+
+                    <h4>Descubre los beneficios de la hidroterapia en tu spa</h4>
+                    <p>La técnica terapéutica de la hidroterapia, tiene muchos beneficios que han sido utilizados para
+                        tratar una
+                        variedad de afecciones y con el objetivo de mejorar…</p>
+                </a>
+                <a href="#" class="card-category-blog-lite gb-card">
+                    <div class="card-category-blog-lite__container">
+                        <div style="background-image: url('/dist/assets/img/card-beneficios1.webp');"
+                            class="card-category-blog-lite__bg">
+                            <img class="card-category-blog-lite__plus" src="/dist/assets/icons/bx-plus.svg" alt="plus">
+                        </div>
+                    </div>
+
+                    <h4>Descubre los beneficios de la hidroterapia en tu spa</h4>
+                    <p>La técnica terapéutica de la hidroterapia, tiene muchos beneficios que han sido utilizados para
+                        tratar una
+                        variedad de afecciones y con el objetivo de mejorar…</p>
+                </a>
+                <a href="#" class="card-category-blog-lite gb-card">
+                    <div class="card-category-blog-lite__container">
+                        <div style="background-image: url('/dist/assets/img/card-beneficios1.webp');"
+                            class="card-category-blog-lite__bg">
+                            <img class="card-category-blog-lite__plus" src="/dist/assets/icons/bx-plus.svg" alt="plus">
+                        </div>
+                    </div>
+
+                    <h4>Descubre los beneficios de la hidroterapia en tu spa</h4>
+                    <p>La técnica terapéutica de la hidroterapia, tiene muchos beneficios que han sido utilizados para
+                        tratar una
+                        variedad de afecciones y con el objetivo de mejorar…</p>
+                </a>
+                <a href="#" class="card-category-blog-lite gb-card">
+                    <div class="card-category-blog-lite__container">
+                        <div style="background-image: url('/dist/assets/img/card-beneficios1.webp');"
+                            class="card-category-blog-lite__bg">
+                            <img class="card-category-blog-lite__plus" src="/dist/assets/icons/bx-plus.svg" alt="plus">
+                        </div>
+                    </div>
+
+                    <h4>Descubre los beneficios de la hidroterapia en tu spa</h4>
+                    <p>La técnica terapéutica de la hidroterapia, tiene muchos beneficios que han sido utilizados para
+                        tratar una
+                        variedad de afecciones y con el objetivo de mejorar…</p>
+                </a>
+            </div>
+            <div class="guias-prestaciones-slider__slider guias-blogPostSlider">
+                <div data-index="0" class="controls-prestaciones-slider__slider controls-blogPostSlider"></div>
+                <div data-index="1" class="controls-prestaciones-slider__slider controls-blogPostSlider"></div>
+                <div data-index="1" class="controls-prestaciones-slider__slider controls-blogPostSlider"></div>
+                <div data-index="2" class="controls-prestaciones-slider__slider controls-blogPostSlider d-lg-none"></div>
+                <div data-index="3" class="controls-prestaciones-slider__slider controls-blogPostSlider d-lg-none"></div>
+            </div>
+        </div>
+    </section>
+
+    <script src="/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="/dist/js/main.js"></script>
+</body>
+
+</html>
+`
