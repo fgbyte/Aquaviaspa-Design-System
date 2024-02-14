5a496c36b51e2ee0d462d58e059bbb2e4d8b9468
fgbyte <fer.garciabayon@gmail.com>
2024-02-14 00:42:16 -0500

    remove steps mark asistente

--- dist/js/main.js
+++ dist/js/main.js
@@ -810,48 +810,47 @@ function handleAnteriorClick() {
 //         })
 //     }
 // });
-var textInStep = document.querySelectorAll('.dynamic-step');
-var radios = document.querySelectorAll('.asistente-radio');
-var firstRadios = document.querySelectorAll('.type-spa-selected-radio');
-var spaSelected = document.querySelectorAll('.type-spa-selected');
-var steps = document.querySelectorAll('.step');
-var stepValue = {
-    step1: '',
-    step2: '',
-    step3: '',
-    step4: '',
-    step5: '',
-};
-function updateTextsFromStepValues() {
-    textInStep.forEach(function (step, index) {
-        var stepKey = "step".concat(index + 1);
-        if (stepValue[stepKey]) {
-            step.innerText = stepValue[stepKey];
-            steps[index].classList.add('step--active');
-        }
-        else {
-            steps[index].classList.remove('step--active');
-        }
-    });
-}
-function setStepValue() {
-    radios.forEach(function (radio, index) {
-        radio.addEventListener('change', function () {
-            var stepIndex = Math.floor(index / 3) + 1;
-            var stepKey = "step".concat(stepIndex);
-            stepValue[stepKey] = radio.value;
-            updateTextsFromStepValues();
-        });
-    });
-}
-firstRadios.forEach(function (radio) {
-    radio.addEventListener('change', function () {
-        spaSelected.forEach(function (element) {
-            element.innerText = radio.value;
-        });
-    });
-});
-setStepValue();
+// const textInStep = document.querySelectorAll('.dynamic-step') as NodeListOf<HTMLElement>;
+// const radios: NodeListOf<HTMLInputElement> = document.querySelectorAll('.asistente-radio');
+// const firstRadios: NodeListOf<HTMLInputElement> = document.querySelectorAll('.type-spa-selected-radio');
+// const spaSelected: NodeListOf<HTMLInputElement> = document.querySelectorAll('.type-spa-selected');
+// const steps = document.querySelectorAll('.step') as NodeListOf<HTMLElement>;
+// const stepValue = {
+//   step1: '',
+//   step2: '',
+//   step3: '',
+//   step4: '',
+//   step5: '',
+// }
+// function updateTextsFromStepValues() {
+//   textInStep.forEach((step, index) => {
+//     const stepKey = `step${index + 1}` as keyof typeof stepValue;
+//     if (stepValue[stepKey]) {
+//       step.innerText = stepValue[stepKey];
+//       steps[index].classList.add('step--active');
+//     } else {
+//       steps[index].classList.remove('step--active');
+//     }
+//   });
+// }
+// function setStepValue() {
+//   radios.forEach((radio, index) => {
+//     radio.addEventListener('change', () => {
+//       const stepIndex = Math.floor(index / 3) + 1;
+//       const stepKey = `step${stepIndex}` as keyof typeof stepValue;
+//       stepValue[stepKey] = radio.value;
+//       updateTextsFromStepValues();
+//     });
+//   });
+// }
+// firstRadios.forEach((radio) => {
+//   radio.addEventListener('change', () => {
+//     spaSelected.forEach((element) => {
+//       element.innerText = radio.value;
+//     });
+//   });
+// });
+// setStepValue();
 var blogPostSlider = document.querySelectorAll(".blog-post-slider");
 var guiasBlogPostSlider = document.querySelectorAll(".guias-blogPostSlider");
 blogPostSlider.forEach(function (carousel, carouselIndex) {
--- theme/pages/asistente/asistente.ts
+++ theme/pages/asistente/asistente.ts
@@ -1,49 +1,49 @@
 
-const textInStep = document.querySelectorAll('.dynamic-step') as NodeListOf<HTMLElement>;
-const radios: NodeListOf<HTMLInputElement> = document.querySelectorAll('.asistente-radio');
-const firstRadios: NodeListOf<HTMLInputElement> = document.querySelectorAll('.type-spa-selected-radio');
-const spaSelected: NodeListOf<HTMLInputElement> = document.querySelectorAll('.type-spa-selected');
-const steps = document.querySelectorAll('.step') as NodeListOf<HTMLElement>;
+// const textInStep = document.querySelectorAll('.dynamic-step') as NodeListOf<HTMLElement>;
+// const radios: NodeListOf<HTMLInputElement> = document.querySelectorAll('.asistente-radio');
+// const firstRadios: NodeListOf<HTMLInputElement> = document.querySelectorAll('.type-spa-selected-radio');
+// const spaSelected: NodeListOf<HTMLInputElement> = document.querySelectorAll('.type-spa-selected');
+// const steps = document.querySelectorAll('.step') as NodeListOf<HTMLElement>;
 
-const stepValue = {
-  step1: '',
-  step2: '',
-  step3: '',
-  step4: '',
-  step5: '',
-}
+// const stepValue = {
+//   step1: '',
+//   step2: '',
+//   step3: '',
+//   step4: '',
+//   step5: '',
+// }
 
 
-function updateTextsFromStepValues() {
-  textInStep.forEach((step, index) => {
-    const stepKey = `step${index + 1}` as keyof typeof stepValue;
-    if (stepValue[stepKey]) {
-      step.innerText = stepValue[stepKey];
+// function updateTextsFromStepValues() {
+//   textInStep.forEach((step, index) => {
+//     const stepKey = `step${index + 1}` as keyof typeof stepValue;
+//     if (stepValue[stepKey]) {
+//       step.innerText = stepValue[stepKey];
 
-      steps[index].classList.add('step--active');
-    } else {
-      steps[index].classList.remove('step--active');
-    }
-  });
-}
+//       steps[index].classList.add('step--active');
+//     } else {
+//       steps[index].classList.remove('step--active');
+//     }
+//   });
+// }
 
-function setStepValue() {
-  radios.forEach((radio, index) => {
-    radio.addEventListener('change', () => {
-      const stepIndex = Math.floor(index / 3) + 1;
-      const stepKey = `step${stepIndex}` as keyof typeof stepValue;
-      stepValue[stepKey] = radio.value;
-      updateTextsFromStepValues();
-    });
-  });
-}
+// function setStepValue() {
+//   radios.forEach((radio, index) => {
+//     radio.addEventListener('change', () => {
+//       const stepIndex = Math.floor(index / 3) + 1;
+//       const stepKey = `step${stepIndex}` as keyof typeof stepValue;
+//       stepValue[stepKey] = radio.value;
+//       updateTextsFromStepValues();
+//     });
+//   });
+// }
 
-firstRadios.forEach((radio) => {
-  radio.addEventListener('change', () => {
-    spaSelected.forEach((element) => {
-      element.innerText = radio.value;
-    });
-  });
-});
+// firstRadios.forEach((radio) => {
+//   radio.addEventListener('change', () => {
+//     spaSelected.forEach((element) => {
+//       element.innerText = radio.value;
+//     });
+//   });
+// });
 
-setStepValue();
+// setStepValue();
